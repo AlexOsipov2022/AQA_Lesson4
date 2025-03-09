c5256733f40de97d3ac0cb63ea8aa2654d4972b0
alexandr-osipov <alexandr.osipov@idfinance.com>
2025-03-09 17:05:34 +0300

    Start commit

--- src/test/java/ui/HomePageTests.java
+++ src/test/java/ui/HomePageTests.java
@@ -1,29 +1,50 @@
 package ui;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class HomePageTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HomePageTests {
 
     WebDriver driver;
+    private static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
 
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         driver.quit();
     }
 
     @Test
     void openHomePageTest() {
-        driver = new ChromeDriver();
-        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
-
+        driver.get(BASE_URL);
         driver.manage().window().maximize();
         String actualTitle = driver.getTitle();
 
-        Assertions.assertEquals("Hands-On Selenium WebDriver with Java", actualTitle);
+        assertEquals("Hands-On Selenium WebDriver with Java", actualTitle);
+    }
 
+    @Test
+    void openWebFormTest() {
+        driver.get(BASE_URL);
+        driver.manage().window().maximize();
+        driver.findElement(By.linkText("Web form")).click();
+        WebElement pageName  = driver.findElement(By.className("display-6"));
+
+        String urlWebFormPage = driver.getCurrentUrl();
+        String currentUrl = "web-form.html";
+
+        assertEquals(BASE_URL + currentUrl, urlWebFormPage);
+        assertEquals("Web form", pageName.getText());
     }
 }
